775ca82f2230ee7376aec9bf48f12b8ba246c628
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-12-03 07:47:50 +0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name = aws_key_pair.ansible.key_name
+  subnet_id = var.instance_subnet
 
   tags = {
     Name = var.instance_name
