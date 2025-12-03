75a2d8998b05f6401cb2f19ffaad535572876831
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-12-03 08:32:37 +0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name = aws_key_pair.ubuntu.key_name
+  key_name = aws_key_pair.jenkins.key_name
   subnet_id = var.instance_subnet
   vpc_security_group_ids = [aws_security_group.init_sg.id]
   user_data                   = file("./user-data")
@@ -16,7 +16,7 @@ resource "aws_instance" "server" {
 
 
   provisioner "local-exec" {
-    command = "sleep 10 && cd ../ansible && ansible-playbook -vv db.yaml -u jenkins -i '${self.private_ip},'"
+    command = "sleep 10 && cd ../ansible && ansible-playbook -vv db.yaml -u ubuntu -i '${self.private_ip},'"
     interpreter = ["/bin/bash", "-c"]
   }
 }
