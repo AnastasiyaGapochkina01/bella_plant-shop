f8882531362bd7984566d2e3fc9add9c245a00fd
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2025-12-03 07:10:22 +0300

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "server" {
   ami           = ami-0ecb62995f68bb549
   instance_type = var.instance_type
+  key_name = var.ssh_public_key_path
 
   tags = {
     Name = var.instance_name
   }
 }
-
-output "instance_ip" { value = aws_instance.server.public_ip }
